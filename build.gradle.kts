463a6d13e6972fc7b6ab713ac9839b273f782862
bnechyporenko <bogdannechiporenko@gmail.com>
2021-09-30 17:08:46 +0200

    ENG-7345 Generate documentation and publish it to GitHub Pages along with release

--- build.gradle.kts
+++ build.gradle.kts
@@ -53,57 +53,44 @@ if (project.hasProperty("sonatypeUsername") && project.hasProperty("public")) {
 
     publishing {
         publications {
-            register("mavenJava", MavenPublication::class) {
-                from(components["java"])
+            afterEvaluate {
+                named<MavenPublication>("mavenJava") {
+                    from(components["java"])
 
-                groupId = PluginConstants.groupId
-                artifactId = PluginConstants.artifactId
-                version = releasedVersion
+                    groupId = PluginConstants.groupId
+                    artifactId = PluginConstants.artifactId
+                    version = releasedVersion
 
-                pom {
-                    name.set("Integration Server Gradle Plugin")
-                    description.set("The easy way to get custom setup for Deploy up and running")
-                    url.set(PluginConstants.repositoryUrl)
-                    licenses {
-                        license {
-                            name.set("GPLv2 with Digital.ai FLOSS License Exception")
-                            url.set(PluginConstants.repositoryLicense)
+                    pom {
+                        name.set("Integration Server Gradle Plugin")
+                        description.set("The easy way to get custom setup for Deploy up and running")
+                        url.set(PluginConstants.repositoryUrl)
+                        licenses {
+                            license {
+                                name.set("GPLv2 with Digital.ai FLOSS License Exception")
+                                url.set(PluginConstants.repositoryLicense)
+                            }
+                        }
+
+                        scm {
+                            url.set(PluginConstants.repositoryScm)
+                        }
+
+                        developers {
+                            developer {
+                                id.set("bnechyporenko")
+                                name.set("Bogdan Nechyporenko")
+                                email.set("bnechyporenko@digital.ai")
+                            }
                         }
                     }
-
-                    scm {
-                        url.set(PluginConstants.repositoryScm)
-                    }
-
-                    developers {
-                        developer {
-                            id.set("bnechyporenko")
-                            name.set("Bogdan Nechyporenko")
-                            email.set("bnechyporenko@digital.ai")
+                    versionMapping {
+                        usage("java-api") {
+                            fromResolutionOf("runtimeClasspath")
                         }
-                        developer {
-                            id.set("aalbul")
-                            name.set("Alexander Albul")
-                            email.set("aalbul@digital.ai")
+                        usage("java-runtime") {
+                            fromResolutionResult()
                         }
-                        developer {
-                            id.set("sishwarya")
-                            name.set("Ishwarya Surendrababu")
-                            email.set("sishwarya@digital.ai")
-                        }
-                        developer {
-                            id.set("tonac")
-                            name.set("Antonio Sostar")
-                            email.set("asostar@digital.ai")
-                        }
-                    }
-                }
-                versionMapping {
-                    usage("java-api") {
-                        fromResolutionOf("runtimeClasspath")
-                    }
-                    usage("java-runtime") {
-                        fromResolutionResult()
                     }
                 }
             }
